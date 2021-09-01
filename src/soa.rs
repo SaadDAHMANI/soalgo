5b97b472a56071affe45ae3d8e8ec17898b12c40
SaadDAHMANI <tahatatakor7@gmail.com>
2021-09-01 15:28:37 +0100

    Initialize repo
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

---
+++ src/soa.rs
@@ -0,0 +1,13 @@
+// Based on : 
+// Gaurav Dhiman , Vijay Kumar, 2018. Seagull optimization algorithm: Theory and its applications for 
+// large-scale industrial engineering problems. Knowledge-Based Systems, volume 165,pages 169--196, 2019. Elsevier.
+// ------------------
+// Gaurav Dhiman (2021). Seagull Optimization Algorithm (SOA) 
+// (https://www.mathworks.com/matlabcentral/fileexchange/75180-seagull-optimization-algorithm-soa),
+// MATLAB Central File Exchange. Retrieved September 1, 2021. 
+
+pub fn soa()->f64 {
+    let best =0.0f64;
+
+    best
+}
