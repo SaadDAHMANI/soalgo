8121251970554c04974afc6577efd06117fcb5c8
SaadDAHMANI <tahatatakor7@gmail.com>
2021-09-01 15:38:00 +0100

    initilization of repo
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+
+include!("soa.rs");
+
 fn main() {
-    println!("Hello, world!");
+    println!("Seagull Optimization Algorithm (SOA)"); 
+    
+    let result = soa();
+
+    println!("Result = {}", result);
+
 }
